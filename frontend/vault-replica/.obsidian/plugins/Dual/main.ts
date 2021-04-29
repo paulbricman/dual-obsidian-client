67b1872cfc54177a77c7673b216d694869c5dc97
paulbricman <paulbricman@protonmail.com>
2021-04-29 17:16:38 +0300

    #54, update recipes.ts

--- frontend/vault-replica/.obsidian/plugins/Dual/main.ts
+++ frontend/vault-replica/.obsidian/plugins/Dual/main.ts
@@ -1,177 +1,213 @@
-import { App, FileSystemAdapter, Notice, Plugin, PluginSettingTab, Setting, Workspace } from 'obsidian';
-import ChatView from 'view';
-import { Utils } from 'utils';
-import { Recipes } from 'recipes';
+import {
+  App,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  Workspace,
+} from "obsidian";
+import ChatView from "view";
+import { Utils } from "utils";
+import { Recipes } from "recipes";
 
 interface MyPluginSettings {
-	customName: string;
-
+  customName: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	customName: 'Dual'
-}
+  customName: "Dual",
+};
 
-export default class MyPlugin extends Plugin {	
-	settings: MyPluginSettings;
+export default class MyPlugin extends Plugin {
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.registerView('chat', (leaf) => {
-			return new ChatView(leaf, this.settings.customName);
-		});
+    this.registerView("chat", (leaf) => {
+      return new ChatView(leaf, this.settings.customName);
+    });
 
-		this.app.workspace.layoutReady && this.initLeaf(this.app.workspace)
-		this.registerEvent(this.app.workspace.on('layout-ready', () => this.initLeaf(this.app.workspace)))
+    this.app.workspace.layoutReady && this.initLeaf(this.app.workspace);
+    this.registerEvent(
+      this.app.workspace.on("layout-ready", () =>
+        this.initLeaf(this.app.workspace)
+      )
+    );
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		this.addCommand({
-			id: 'focus-dual-input',
-			name: 'Focus Dual input box',
-			callback: () => {
-				document.getElementById('dual-input-box').focus();
-			}
-		});
-	}
+    this.addCommand({
+      id: "focus-dual-input",
+      name: "Focus Dual input box",
+      callback: () => {
+        document.getElementById("dual-input-box").focus();
+      },
+    });
+  }
 
-	initLeaf(workspace: Workspace): void {
-		if (workspace.getLeavesOfType('chat').length == 0) {
-			workspace.getRightLeaf(false).setViewState({
-				type: 'chat'
-			})
-		}
-	}
+  initLeaf(workspace: Workspace): void {
+    if (workspace.getLeavesOfType("chat").length == 0) {
+      workspace.getRightLeaf(false).setViewState({
+        type: "chat",
+      });
+    }
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.app = app;
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.app = app;
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h3', {text: 'Follow these instructions to set up your Dual:'});
+    containerEl.createEl("h3", {
+      text: "Follow these instructions to set up your Dual:",
+    });
 
-		new Setting(containerEl)
-			.setName('RECIPE TEST')
-			.addButton(cb => cb
-				.setButtonText('Test')
-				.setClass('mod-cta')
-				.onClick(() => {
-					Recipes.getIngredients('Come up with an application of blockchain in advertising.', ['technology', 'application domain'])
-						.then((res) => {
-							console.log(res);
-						})
-				}));
+    new Setting(containerEl).setName("RECIPE TEST").addButton((cb) =>
+      cb
+        .setButtonText("Test")
+        .setClass("mod-cta")
+        .onClick(() => {
+            console.log(Recipes.resolveIngredientNames("An application of *technology* in *application domain*:", ["technology", "application domain"], ["blockchain", "advertising"]))
+        })
+    );
 
-		new Setting(containerEl)
-		.setName('0. Install Python (3.8+).')
-		.setDesc('Press the button to head over to the download page.')
-		.addButton(cb => cb
-			.setButtonText('Install Python')
-			.setClass('mod-cta')
-			.onClick(() => {
-				window.open('https://www.python.org/downloads/')
-			}));
+    new Setting(containerEl)
+      .setName("0. Install Python (3.8+).")
+      .setDesc("Press the button to head over to the download page.")
+      .addButton((cb) =>
+        cb
+          .setButtonText("Install Python")
+          .setClass("mod-cta")
+          .onClick(() => {
+            window.open("https://www.python.org/downloads/");
+          })
+      );
 
-		new Setting(containerEl)
-			.setName('1. Copy snapshot.')
-			.setDesc('Press the button to copy the entire vault as concatenated plain text.')
-			.addButton(cb => cb
-				.setButtonText('Copy snapshot')
-				.setClass('mod-cta')
-				.onClick(() => {
-					new Notice('Loading files...');
+    new Setting(containerEl)
+      .setName("1. Copy snapshot.")
+      .setDesc(
+        "Press the button to copy the entire vault as concatenated plain text."
+      )
+      .addButton((cb) =>
+        cb
+          .setButtonText("Copy snapshot")
+          .setClass("mod-cta")
+          .onClick(() => {
+            new Notice("Loading files...");
 
-					let concatenated = '';
+            let concatenated = "";
 
-					this.app.vault.getMarkdownFiles().forEach(element => {
-						this.app.vault.cachedRead(element)
-							.then((res) => {
-								res = res
-									.replace(/^---[\s\S]*---\n*/g, '')
-									.replace(/\[\[[^\|\[\]]*\|([^\|\[\]]*)\]\]/g, '$1')
-									.replace(/\[\[(.*)\]\]/g, '$1')
-									.replace(/```([^`])*```\n*/g, '')
-									.replace(/\$([^$])*\$*/g, '')
-									
-								concatenated = concatenated.concat(res, '\n\n');
-							});
-					});
+            this.app.vault.getMarkdownFiles().forEach((element) => {
+              this.app.vault.cachedRead(element).then((res) => {
+                res = res
+                  .replace(/^---[\s\S]*---\n*/g, "")
+                  .replace(/\[\[[^\|\[\]]*\|([^\|\[\]]*)\]\]/g, "$1")
+                  .replace(/\[\[(.*)\]\]/g, "$1")
+                  .replace(/```([^`])*```\n*/g, "")
+                  .replace(/\$([^$])*\$*/g, "");
 
-					let copyPromise = new Promise(resolve => setTimeout(resolve, 3000)).then(() => {
-						concatenated = concatenated.slice(0, 5000000);
-						concatenated = Utils.removeMd(concatenated, {});
-						Utils.copyStringToClipboard(concatenated);
-						new Notice('Snapshot successfully copied to clipboard!');
-					});
-				}));
+                concatenated = concatenated.concat(res, "\n\n");
+              });
+            });
 
-		new Setting(containerEl)
-			.setName('2. Derive the essence.')
-			.setDesc('After following the online instructions, extract \'essence.zip\' in \'.obsidian/plugins/Dual/\'.')
-			.addButton(cb => cb
-				.setButtonText('Start alignment')
-				.setClass('mod-cta')
-				.onClick(() => {
-					window.open('https://colab.research.google.com/drive/1CObehan5gmYO-TvyyYq973a3h-_EYr9_?usp=sharing')
-				}));
+            let copyPromise = new Promise((resolve) =>
+              setTimeout(resolve, 3000)
+            ).then(() => {
+              concatenated = concatenated.slice(0, 5000000);
+              concatenated = Utils.removeMd(concatenated, {});
+              Utils.copyStringToClipboard(concatenated);
+              new Notice("Snapshot successfully copied to clipboard!");
+            });
+          })
+      );
 
-		new Setting(containerEl)
-			.setName('3. Configure the skeleton.')
-			.setDesc('Run \'python3 -m pip install -r requirements.txt\' in \'.obsidian/plugins/Dual/skeleton/\'.');
+    new Setting(containerEl)
+      .setName("2. Derive the essence.")
+      .setDesc(
+        "After following the online instructions, extract 'essence.zip' in '.obsidian/plugins/Dual/'."
+      )
+      .addButton((cb) =>
+        cb
+          .setButtonText("Start alignment")
+          .setClass("mod-cta")
+          .onClick(() => {
+            window.open(
+              "https://colab.research.google.com/drive/1CObehan5gmYO-TvyyYq973a3h-_EYr9_?usp=sharing"
+            );
+          })
+      );
 
-		new Setting(containerEl)
-			.setName('4. Run the skeleton after you configured the essence.')
-			.setDesc('Run \'python3 server.py --path /path/to/your/vault/\' in \'.obsidian/plugins/Dual/skeleton/\'.');
+    new Setting(containerEl)
+      .setName("3. Configure the skeleton.")
+      .setDesc(
+        "Run 'python3 -m pip install -r requirements.txt' in '.obsidian/plugins/Dual/skeleton/'."
+      );
 
-		new Setting(containerEl)
-			.setName('5. Restart Obsidian.')
-			.setDesc('Head over to the right side panel to talk with your Dual!');
+    new Setting(containerEl)
+      .setName("4. Run the skeleton after you configured the essence.")
+      .setDesc(
+        "Run 'python3 server.py --path /path/to/your/vault/' in '.obsidian/plugins/Dual/skeleton/'."
+      );
 
-		containerEl.createEl('h3', {text: 'Congratulations on setting up your Dual!'});
+    new Setting(containerEl)
+      .setName("5. Restart Obsidian.")
+      .setDesc("Head over to the right side panel to talk with your Dual!");
 
-		new Setting(containerEl)
-		.setName('Custom name')
-		.setDesc('Customize your Dual\'s name using the input box. Reload Obsidian for this to take effect.')
-		.addText(text => text
-			.setPlaceholder('Dual')
-			.setValue('')
-			.onChange(async (value) => {
-				this.plugin.settings.customName = value;
-				await this.plugin.saveSettings();
-			}));
-		
-		new Setting(containerEl)
-			.setName('Get involved!')
-			.addButton(cb => cb
-				.setButtonText('Report bugs')
-				.setClass('mod-cta')
-				.onClick(() => {
-					window.open('https://github.com/Psionica/Dual/issues')
-				}))
-			.addButton(cb => cb
-				.setButtonText('Join Psionica')
-				.setClass('mod-cta')
-				.onClick(() => {
-					window.open('https://psionica.org/')
-				}))
-	}
+    containerEl.createEl("h3", {
+      text: "Congratulations on setting up your Dual!",
+    });
+
+    new Setting(containerEl)
+      .setName("Custom name")
+      .setDesc(
+        "Customize your Dual's name using the input box. Reload Obsidian for this to take effect."
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder("Dual")
+          .setValue("")
+          .onChange(async (value) => {
+            this.plugin.settings.customName = value;
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Get involved!")
+      .addButton((cb) =>
+        cb
+          .setButtonText("Report bugs")
+          .setClass("mod-cta")
+          .onClick(() => {
+            window.open("https://github.com/Psionica/Dual/issues");
+          })
+      )
+      .addButton((cb) =>
+        cb
+          .setButtonText("Join Psionica")
+          .setClass("mod-cta")
+          .onClick(() => {
+            window.open("https://psionica.org/");
+          })
+      );
+  }
 }
--- frontend/vault-replica/.obsidian/plugins/Dual/recipes.ts
+++ frontend/vault-replica/.obsidian/plugins/Dual/recipes.ts
@@ -1,86 +1,139 @@
-import { App } from 'obsidian';
+import { App } from "obsidian";
 
 export module Recipes {
-    export function getExamples(app: App) {
-        var examples: string[] = [];
-        var paths: string[] = [];
+  // Get two parallel lists of query examples and the paths they originate from
+  export function getExamples(app: App) {
+    var examples: string[] = [];
+    var paths: string[] = [];
 
-        app.vault.getMarkdownFiles().forEach(file => {
-            if (file.path.startsWith("dual-recipes")) {
-                app.metadataCache.getFileCache(file).frontmatter['examples'].forEach((example: string) => {
-                    examples = examples.concat(example);
-                    paths = paths.concat(file.path);
-                });
-            }
-        });
-        
-        return [examples, paths]        
+    app.vault.getMarkdownFiles().forEach((file) => {
+      if (file.path.startsWith("dual-recipes")) {
+        app.metadataCache
+          .getFileCache(file)
+          .frontmatter["examples"].forEach((example: string) => {
+            examples = examples.concat(example);
+            paths = paths.concat(file.path);
+          });
+      }
+    });
+
+    return [examples, paths];
+  }
+
+  // Get contents of a recipe at a path
+  export async function recipeContents(app: App, path: string) {
+    var markdownFiles = app.vault.getMarkdownFiles();
+
+    for (let index = 0; index < markdownFiles.length; index++) {
+      if (markdownFiles[index].path == path) {
+        return await app.vault.cachedRead(markdownFiles[index]);
+      }
+    }
+  }
+
+  // Find closest example to a given query
+  export async function matchQuery(app: App, query: string) {
+    var examplePathPairs = this.getExamples(app);
+    var examples = examplePathPairs[0],
+      paths = examplePathPairs[1];
+
+    const rawResponse = await fetch("http://127.0.0.1:5000/extract/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        documents: examples,
+        selected_candidates: 1,
+      }),
+    });
+
+    var content = await rawResponse.json();
+    return paths[content["output"][0]];
+  }
+
+  // Substitute ingredient names with actual ingredients in the recipe
+  export function resolveIngredientNames(recipeContents: string, ingredientNames: string[], ingredients: string[]) {
+    for (let index = 0; index < ingredientNames.length; index++) {
+        var re = RegExp("\\*" + ingredientNames[index] + "\\*", "g")
+        recipeContents = recipeContents.replace(re, ingredients[index])
     }
 
-    export async function matchQuery(app: App, query: string) {
-        var examplePathPairs = this.getExamples(app);
-        var examples = examplePathPairs[0], paths = examplePathPairs[1];
+    return recipeContents
+  }
 
-        const rawResponse = await fetch('http://127.0.0.1:5000/extract/', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({query: query, documents: examples, selected_candidates: 1})
-        });
+  export function removeFrontMatter(recipeContents: string) {
+    recipeContents = recipeContents.replace(/---[^-]*---/g, "")
+    return recipeContents.trim() 
+  }
 
-        var content = await rawResponse.json();
-        return paths[content['output'][0]];
+  export async function followRecipe(app: App, path: string, query: string) {
+    var recipeContents: string = await getRecipeContents(app, path);
+    var ingredientNames: string[] = await getIngredientNames(app, recipeContents);
+    var ingredients: string[] = ["blockchain", "advertising"];//await getIngredients(query, ingredientNames);
+
+    recipeContents = removeFrontMatter(recipeContents)
+    recipeContents = resolveIngredientNames(recipeContents, ingredientNames, ingredients);
+  }
+
+  export function splitRecipe(recipeContents: string) {
+
+  }
+
+  // Get list of ingredient names mentioned in a recipe
+  export async function getIngredientNames(app: App, recipeContents: string) {
+    var re = /\*[^\*]*\*/g;
+    var ingredientNames = recipeContents.match(re);
+
+    ingredientNames.forEach((val, index, ingredientNames) => {
+      ingredientNames[index] = val.substring(1, val.length - 1);
+    });
+
+    return ingredientNames;
+  }
+
+  // Parse actual ingredients from the query
+  export async function getIngredients(
+    query: string,
+    ingredientNames: string[]
+  ) {
+    var ingredients: string[] = [],
+      res;
+
+    for (let index = 0; index < ingredientNames.length; index++) {
+      res = await this.getIngredient(query, ingredientNames[index]);
+      res = res.split('"')[0];
+      ingredients = ingredients.concat(res);
     }
 
-    export function getIngredientNames(app: App, path: string) {
-        var re = /\*[^\*]*\*/g;
-        var ingredientNames: string[];
+    return ingredients;
+  }
 
-        app.vault.getMarkdownFiles().forEach(file => {
-            if (file.path == path) {
-                app.vault.cachedRead(file).then((res) => {
-                    ingredientNames = res.match(re)
-                    ingredientNames.forEach((val, index, ingredientNames) => {
-                        ingredientNames[index] = val.substring(1, val.length - 1);
-                    });
-                    return ingredientNames;
-                })
-            }
-        })
-    }
+  // Parse one actual ingredient from the query
+  export async function getIngredient(query: string, ingredientName: string) {
+    var prompt: string =
+      getIngredientPrompt + query + "\n" + ingredientName + ': "';
+    const rawResponse = await fetch("http://127.0.0.1:5000/generate/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        early_stopping_criterion: "finish_paragraph",
+        max_generated_token_count: Math.floor(query.length * 0.3),
+      }),
+    });
 
-    export async function getIngredients(query: string, ingredientNames: string[]) {
-        var ingredients: string[] = [], res;
+    var content = await rawResponse.json();
+    content = content["output"][0];
+    return content;
+  }
 
-        for (let index = 0; index < ingredientNames.length; index++) {
-            res = await this.getIngredient(query, ingredientNames[index]);
-            res = res.split("\"")[0];
-            ingredients = ingredients.concat(res);
-        }
-
-        return ingredients;
-    }
-
-    export async function getIngredient(query: string, ingredientName: string) {
-        var prompt: string = getIngredientPrompt + query + '\n' + ingredientName + ': "'
-        const rawResponse = await fetch('http://127.0.0.1:5000/generate/', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({prompt: prompt, early_stopping_criterion:'finish_paragraph', max_generated_token_count:Math.floor(query.length * 0.3)})
-        });
-
-        var content = await rawResponse.json();
-        content = content['output'][0];
-        return content
-    }
-
-    const getIngredientPrompt: string =
-`query: Come up with a writing prompt about aliens and robots.
+  const getIngredientPrompt: string = `query: Come up with a writing prompt about aliens and robots.
 topic: "aliens and robots"
 
 query: Einstein, what is general relativity?
@@ -125,5 +178,5 @@ topic: "evolution"
 query: Isaac Asimov, come up with a writing prompt about space exploration.
 person: "Isaac Asimov" 
 
-query: `
+query: `;
 }
