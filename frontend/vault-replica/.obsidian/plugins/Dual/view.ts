cdeae276f32df49b791d2b6b7abe3019e179c6a7
benjamin <benjamin@leveritt.co.uk>
2021-06-28 08:36:56 +0100

    Removes unused inline styles

--- frontend/vault-replica/.obsidian/plugins/Dual/view.ts
+++ frontend/vault-replica/.obsidian/plugins/Dual/view.ts
@@ -78,58 +78,21 @@ export default class ChatView extends ItemView {
 
     let header = headerDiv.createEl("h3");
     header.appendText(this.customName);
-    // header.style.textAlign = "left";
-    // header.style.marginTop = "0px";
-    // header.style.marginBottom = "0px";
-    // header.style.position = "absolute";
-    // header.style.top = "15px";
 
     let status = headerDiv.createEl("h6");
     status.id = "status";
     status.appendText("online");
-    // status.style.textAlign = "left";
-    // status.style.marginTop = "0px";
-    // status.style.marginBottom = "5px";
-    // status.style.color = "grey";
 
     let conversationDiv = headerDiv.createDiv({ cls: "nav-header" });
     conversationDiv.id = "conversationDiv";
-    // conversationDiv.style.padding = "0";
-    // conversationDiv.style.backgroundColor = "var(--background-secondary-alt)";
-    // conversationDiv.style.position = "absolute";
-    // conversationDiv.style.left = "0";
-    // conversationDiv.style.width = "100%";
-    // conversationDiv.style.paddingLeft = "10px";
-    // conversationDiv.style.paddingRight = "10px";
-    // conversationDiv.style.overflowY = "scroll";
-    // conversationDiv.style.height = "calc(100% - 110px)";
 
     let input = footerDiv.createEl("input");
     input.id = "dual-input-box";
     input.type = "text";
-    // input.style.fontSize = "0.8em";
-    // input.style.paddingInlineStart = "2%";
-    // input.style.paddingInlineEnd = "2%";
-    // input.style.marginTop = "0px";
-    // input.style.marginBottom = "10px";
-    // input.style.maxWidth = "68%";
-    // input.style.minWidth = "68%";
-    // input.style.position = "absolute";
-    // input.style.bottom = "0";
-    // input.style.left = "5%";
 
     let button = footerDiv.createEl("button");
     button.appendText("Send");
     button.id = "send-button";
-    // button.style.alignItems = "left";
-    // button.style.paddingInlineStart = "2%";
-    // button.style.paddingInlineEnd = "2%";
-    // button.style.marginTop = "0px";
-    // button.style.marginBottom = "10px";
-    // button.style.width = "20%";
-    // button.style.position = "absolute";
-    // button.style.bottom = "0";
-    // button.style.left = "75%";
 
     this.registerDomEvent(button, "click", () => this.sendMessage());
     this.registerDomEvent(input, "keydown", (event) => {
@@ -148,9 +111,6 @@ export default class ChatView extends ItemView {
     );
     let p = conversationDiv.createEl("p");
 
-    // p.style.boxSizing = "border-box";
-    // p.style.maxWidth = "90%";
-
     MarkdownRenderer.renderMarkdown(
       content,
       p,
@@ -165,16 +125,6 @@ export default class ChatView extends ItemView {
       );
     }
 
-    // p.style.userSelect = "text";
-    // p.style.textAlign = "left";
-    // p.style.fontSize = "0.8em";
-    // p.style.borderRadius = "5px";
-    // p.style.lineHeight = "18px";
-    // p.style.padding = "0px";
-    // p.style.paddingBlockStart = "0px";
-    // p.style.marginTop = "5px";
-    // p.style.marginBottom = "5px";
-
     if (side == "right") {
       p.style.backgroundColor = "var(--background-primary)";
     } else {
@@ -186,9 +136,6 @@ export default class ChatView extends ItemView {
     } else {
       p.style.float = "left";
     }
-
-    // p.style.display = "inline-block";
-    // p.style.clear = "both";
 
     conversationDiv.scrollBy(0, 1000);
   }
