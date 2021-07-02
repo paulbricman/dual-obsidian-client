72a172f5ab9514506e402869ed6fd66f8ec605ab
paulbricman <paulbricman@protonmail.com>
2021-07-02 17:35:44 +0200

    Remove backend

--- backend/src/utils.rs
+++
@@ -1,9 +0,0 @@
-/// Return position of subsequence in sequence
-pub fn find_subsequence<T>(haystack: &[T], needle: &[T]) -> Option<usize>
-where
-    for<'a> &'a [T]: PartialEq,
-{
-    haystack
-        .windows(needle.len())
-        .position(|window| window == needle)
-}
