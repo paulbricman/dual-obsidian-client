f3c4bb02a2b317cb29839fd0e7d10671d85459a3
benjamin <benjamin@leveritt.co.uk>
2021-07-17 09:28:55 +0100

    Adds output to test vault

--- rollup.config.js
+++ rollup.config.js
@@ -14,13 +14,22 @@ if you want to view the source visit the plugins github repository
 
 export default {
   input: "src/main.ts",
-  output: {
-    dir: "dist/Dual",
-    sourcemap: !production && "inline",
-    format: "cjs",
-    exports: "default",
-    banner,
-  },
+  output: [
+    {
+      dir: "dist/Dual",
+      sourcemap: !production && "inline",
+      format: "cjs",
+      exports: "default",
+      banner,
+    },
+    {
+      dir: "frontend/vault-replica/.obsidian/plugins/Dual",
+      sourcemap: !production && "inline",
+      format: "cjs",
+      exports: "default",
+      banner,
+    },
+  ],
   external: ["obsidian"],
   plugins: [
     typescript({
